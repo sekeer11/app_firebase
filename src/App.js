4a0c9a709dc992e1805ef3c0dd36c7ebd121061e
seeker11 <jmesabong@gmail.com>
2023-03-21 21:44:39 -0400

    Iniciando proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return ( 
+        <Contenedor>
+            <Titulo>Lista de contactos</Titulo>
+        </Contenedor>
+     );
 }
-
+ 
+const Contenedor = styled.div`
+    margin: 40px;
+    width: 90%;
+    max-width: 400px;
+    background: #fff;
+    padding: 40px;
+    border-radius: 5px;
+    text-align: center;
+`;
+ 
+const Titulo = styled.h2`
+    margin-bottom: 10px;
+`;
 export default App;
